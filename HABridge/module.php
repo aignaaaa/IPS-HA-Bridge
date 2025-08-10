dfbee5c3d9a1a37d5d5ed7dd0fea529c354a77e3
aignaaaa <50040452+aignaaaa@users.noreply.github.com>
2025-08-10 09:05:36 +0200

    Update module.php

--- HABridge/module.php
+++ HABridge/module.php
@@ -3,16 +3,20 @@ declare(strict_types=1);
 
 class HAAuto extends IPSModule
 {
+    // Expliziter, korrekter Konstruktor (nimmt $InstanceID entgegen und reicht sie weiter)
+    public function __construct($InstanceID)
+    {
+        parent::__construct($InstanceID);
+    }
+
     public function Create(): void
     {
         parent::Create();
-        // Beispiel-Property (optional)
         $this->RegisterPropertyString('Note', '');
     }
 
     public function ApplyChanges(): void
     {
         parent::ApplyChanges();
-        // Keine Parent-Verbindung, noch minimal
     }
 }

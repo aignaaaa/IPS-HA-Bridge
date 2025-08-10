d52dc19181b1a55132e425719e4850c22fec07d9
aignaaaa <50040452+aignaaaa@users.noreply.github.com>
2025-08-10 08:49:15 +0200

    Update module.php

--- HABridge/module.php
+++ HABridge/module.php
@@ -1,16 +1,8 @@
 <?php
 declare(strict_types=1);
 
-class HABridge extends IPSModule
+class HAAuto extends IPSModule
 {
-    public function Create(): void
-    {
-        parent::Create();
-        $this->RegisterPropertyString('Note', '');
-    }
-
-    public function ApplyChanges(): void
-    {
-        parent::ApplyChanges();
-    }
+    public function Create(): void { parent::Create(); }
+    public function ApplyChanges(): void { parent::ApplyChanges(); }
 }
